46798e7b123af2cc3734a36bcd259222a9d63f5c
dwann <143673471+dajhtugas@users.noreply.github.com>
2024-02-24 09:50:35 +0800

    Add files via upload

---
+++ q3labactivity_Li_dajhtugas/script.js
@@ -0,0 +1,15 @@
+var a = prompt("Enter A Number: "," ");
+var b = prompt("Enter another number: ", " ");
+parseInt(a);
+parseInt(b);
+var sum = a+b;
+alert(sum)
+
+if(a=b){
+    var patas = "Both values are equal";
+}
+else{
+    var patas = "Both values are unequal";
+}
+
+
